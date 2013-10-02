a5c4edd53f6c10cf6ec7937ab9c43f81267d1dd6
Paul Hadfield <paul@songkick.com>
2013-10-02 16:39:57 +0100

    Fix warnings; more whitespace clean-up.

--- 1_arrays_strings/1_5_string_compression/string_compression.c
+++ 1_arrays_strings/1_5_string_compression/string_compression.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 typedef unsigned short uint8;
 
@@ -10,32 +11,19 @@ typedef enum {
   EInRepeat = 1
 } State;
 
-char emit(char ch) {
-  /* printf("Emit: %c (%d)\n", ch, (uint8)ch); */
-  return ch;
-}
-
-void emitModify(char ch) {
-  /* printf("Updated: %c (%d)\n", ch, (uint8)ch); */
-}
-
 char* compressString(const char* in) {
   char* out = (char*) malloc(1 + strlen(in));
   memset(out, 0, 1 + strlen(in));
 
   State state = EInLiteral;
-    int repeatCount = 0;
+  int repeatCount = 0;
   const char* iptr = in;
   char previous = '\0';
 
   char* optr = out;
-    int lastTagIndex = -1;
+  int lastTagIndex = -1;
 
   while (*iptr) {
-    /* printf("In: %c Previous: %c State: %s RptCnt: %d LastTagIdx: %d\n",
-      *iptr, previous, (state == EInLiteral ? "inlit" : "inrpt"),
-      repeatCount, lastTagIndex);  
-    */
     if (*iptr == previous) {
       switch(state) {
         case EInLiteral:
@@ -44,8 +32,8 @@ char* compressString(const char* in) {
           // the repeat.
           --optr;
           lastTagIndex = optr - out;
-          *optr++ = emit(REPEAT_START);
-          *optr++ = emit(*iptr);
+          *optr++ = REPEAT_START;
+          *optr++ = *iptr;
           repeatCount = 2;
           break;
         case EInRepeat:
@@ -58,19 +46,17 @@ char* compressString(const char* in) {
           break;
         case EInRepeat:
           *(out + lastTagIndex) += repeatCount;
-          emitModify(*(out + lastTagIndex));
           state = EInLiteral;
           repeatCount = 0;
           break;
       }
-      *optr++ = emit(*iptr);
+      *optr++ = *iptr;
     }
     previous = *iptr++;
   }
   // Finally update last tag if in repeat
   if (state == EInRepeat) {
     *(out + lastTagIndex) +=  repeatCount;
-    emitModify(*(out + lastTagIndex));
   }
 
   return out;
@@ -79,7 +65,7 @@ char* compressString(const char* in) {
 void test(const char* input, const char* expectedCompressed) {
   char* compressed = compressString(input);
   if (strcmp(compressed, expectedCompressed) == 0) {
-    printf("PASS: orig len %d; compressed len %d\n", strlen(input), strlen(compressed));
+    printf("PASS: orig len %lu; compressed len %lu\n", strlen(input), strlen(compressed));
   } else {
     printf("FAIL: got %s; expected %s\n", compressed, expectedCompressed);
   }
