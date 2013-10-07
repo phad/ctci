3aa429e90599b3a8781d20a733aef6ecf744baf0
Paul Hadfield <paul@songkick.com>
2013-10-07 13:55:30 +0100

    Find existence of paths between two nodes in an undirected graph.

---
+++ 4_trees_and_graphs/4_2_find_route_between_nodes/find_route_in_undirected_graph.cc
@@ -0,0 +1,60 @@
+#include <assert.h>
+#include <map>
+#include "../undirected_graph.h"
+
+using namespace std;
+
+enum ETraverseState {
+  ENotVisited,
+  EVisiting,
+  EVisited
+};
+
+bool pathExists(const Graph& g, int n1, int n2) {
+  map<int, ETraverseState> traversal;
+  vector<int> toVisitList;
+  for (int n = 0; n < g.numVertices(); ++n) {
+    traversal[n] = ENotVisited;
+  }
+  toVisitList.push_back(n1);
+  bool exhausted = false;
+  while (!exhausted && !toVisitList.empty()) {
+    int nextVertex = toVisitList.back();
+    toVisitList.pop_back();
+    traversal[nextVertex] = EVisiting;
+    set<int> adjList = g.adjacencyList(nextVertex);
+    for (set<int>::iterator it = adjList.begin(); it != adjList.end(); ++it) {
+      if (traversal[*it] == ENotVisited) {
+        if (*it == n2) {
+          return true;
+        }
+        toVisitList.push_back(*it);
+      }
+    }
+    traversal[nextVertex] = EVisited;
+
+    exhausted = true;
+    for (map<int, ETraverseState>::iterator it = traversal.begin();
+         it != traversal.end(); ++it) {
+      exhausted &= (it->second == EVisited);
+    }
+  }
+  return false;
+}
+
+int main(int argc, char **argv) {
+  Graph g(8);
+  g.addEdge(0, 1);
+  g.addEdge(0, 2);
+  g.addEdge(0, 3);
+  g.addEdge(3, 4);
+  g.addEdge(4, 5);
+
+  assert(pathExists(g, 0, 1));
+  assert(pathExists(g, 0, 4));
+  assert(pathExists(g, 1, 5));
+  assert(!pathExists(g, 3, 6));
+  assert(!pathExists(g, 6, 7));
+
+  return 0;
+}
